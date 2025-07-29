b495e72e26c5be641381e0c6a68293ce3293bf7a
Alhadis <gardnerjohng@gmail.com>
2025-07-29 10:06:56 +1000

    Define additional AppleScript doc sources

--- config.mk
+++ config.mk
@@ -3,4 +3,9 @@
 # User-configurable parts of the project's build system
 DOCS  := Docs
 BOOKS := https://developer.apple.com/library/archive/documentation/AppleScript/Conceptual/AppleScriptLangGuide/book.json
+BOOKS += https://developer.apple.com/library/archive/documentation/AppleScript/Conceptual/AppleScriptX/book.json
+BOOKS += https://developer.apple.com/library/archive/documentation/AppleScript/Conceptual/AppleEvents/book.json
+BOOKS += https://developer.apple.com/library/archive/documentation/AppleScript/Conceptual/StudioBuildingApps/book.json
+BOOKS += https://developer.apple.com/library/archive/documentation/AppleScript/Conceptual/soapXMLRPC/book.json
 BOOKS += https://developer.apple.com/library/archive/releasenotes/AppleScript/RN-AppleScript/book.json
+BOOKS += https://developer.apple.com/library/archive/releasenotes/AppleScript/RN-AppleScriptStudio/book.json
